cdd5888d8c1d9a053bf045732b0ca58dad1d69da
Ibrahim Abdulkadir <lethalskillzz@gmail.com>
2016-09-28 19:06:07 +0100

    Update algo_tests.py

--- ALGO LAB/algo_tests.py
+++ ALGO LAB/algo_tests.py
@@ -8,7 +8,7 @@ class AlgorithmTestCases(unittest.TestCase):
   def test_maximum_number_two(self):
     result = get_algorithm_result(["apples", "oranges", "mangoes", "banana", "zoo"])
     self.assertEqual(result, "zoo", msg="Incorrect value")
-
+   
   def test_prime_number_one(self):
     result = prime_number(1)
     self.assertEqual(result, False, msg="Result is invalid")
